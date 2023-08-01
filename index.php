6c298757030c71585bec8d8fcf64c2bd008bae5c
ovarz <ovarz_03@yahoo.com>
2023-08-01 20:28:42 +0700

    update OG

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
 <?php require ('inc/base.php')?>
 <?php 
-  $video_content='QcI7Vapk05o'; 
-  $video_title='Mobil Baru yang ( kemungkinan ) di Jual di GIIAS 2023'; 
-  $video_desc='Event GIIAS 2023 sudah sebentar lagi, akan ada banyak mobil baru yang akan di jual ke pasar indonesia, kira-kira mobil baru apa aja sih yang bakalan di jual di GIIAS 2023, langsung aja nonton video kita ya.';
+  $video_content='zvfRG72kdbU'; 
+  $video_title='Mobil Hybrid Honda Siap Meluncur di GIIAS 2023 ?'; 
+  $video_desc='Hi OG Friends, GIIAS 2023 Sudah sebentar lagi nih, kabarnya pabrikan satu ini akan meluncurkan mobil hybridnya nanti pas event GIIAS 2023, kira-kira Mobil Hybrid Honda apa aja yang akan meluncur di GIIAS 2023, langsung aja tonton video kita ya.';
   
   $video_array = array();
+  $video_array[]=array(
+    'video_id'=>'14',
+    'video_title'=>'Review Hyundai Tucson Tahun 2012 | SUV Hyundai yang Menjadi Incaran Anak Millenial',
+    'video_link'=>'eO7wC4sKN5I'
+  );
+  $video_array[]=array(
+    'video_id'=>'13',
+    'video_title'=>'Mobil Baru yang ( kemungkinan ) di Jual di GIIAS 2023',
+    'video_link'=>'QcI7Vapk05o'
+  );
   $video_array[]=array(
     'video_id'=>'12',
     'video_title'=>'Ada Apa Aja di Event PARJO 2023? | Ngobrol Bareng Founder Parjo',
@@ -19,16 +29,6 @@
     'video_id'=>'10',
     'video_title'=>'Review Mercedes Benz W140 - S 300 | Sedan Para Pemimpin Dunia',
     'video_link'=>'N4upWFINjl4'
-  );
-  $video_array[]=array(
-    'video_id'=>'9',
-    'video_title'=>'Fakta Honda Zoomer atau Honda Ruckus si Skuter Eksibisionis',
-    'video_link'=>'85sNYxCvmoM'
-  );
-  $video_array[]=array(
-    'video_id'=>'8',
-    'video_title'=>'Mau Punya Honda Zoomer Tapi Duit Ngepas? Custom Aja!',
-    'video_link'=>'I6-0N4xktGI'
   );
 ?>
 <?php require ($_SERVER['OG'].'inc/meta.php')?>

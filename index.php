26b5b37753ed533af5741044b6f9bf039c752a32
ovarz <ovarz_03@yahoo.com>
2023-08-13 15:05:34 +0700

    og

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
 <?php require ('inc/base.php')?>
 <?php 
-  $video_content='lbgda5vTFgk'; 
-  $video_title='Review Suzuki Katana Tahun 96 | Jeep Tahun 90an yang Masih Menjadi Incaran Offroader'; 
-  $video_desc='Hi OG Friends, Pada video sebelumnya kita pernah komparasi Suzuki Katana VS Veroza, Nah kali ini kita mau review suzuki katananya Om Acong @primo116garage6 Mau tau gimana suzuki katana ini, langsung aja tonton video kita ya.';
+  $video_content='7ZW9ZFtgx_E'; 
+  $video_title='Ada Mobil Baru Apa Aja di GIIAS 2023 ?'; 
+  $video_desc='OG Friends, GIIAS 2023 sudah dimulai dan OtoGadget sempat berkunjung kesana belum lama ini. Ikuti pantauan kita di #GIIAS2023 ini dimana banyak #mobilbaru dan seru muncul disana Hi OG Friends, GIIAS 2023 udah tiba, kira-kira ada Mobil Baru Apa aja di GIIAS 2023? Langsung aja tonton video kita di GIIAS 2023 ya.';
   
   $video_array = array();
+  $video_array[]=array(
+    'video_id'=>'17',
+    'video_title'=>'GWM Group akan hadir di GIIAS 2023 ? Mobil Listrik China di GIIAS 2023',
+    'video_link'=>'SgccR6ZmNd8'
+  );
+  $video_array[]=array(
+    'video_id'=>'16',
+    'video_title'=>'Review Suzuki Katana Tahun 96 | Jeep Tahun 90an yang Masih Menjadi Incaran Offroader',
+    'video_link'=>'lbgda5vTFgk'
+  );
   $video_array[]=array(
     'video_id'=>'15',
     'video_title'=>'Mobil Hybrid Honda Siap Meluncur di GIIAS 2023 ?',
@@ -19,16 +29,6 @@
     'video_id'=>'13',
     'video_title'=>'Mobil Baru yang ( kemungkinan ) di Jual di GIIAS 2023',
     'video_link'=>'QcI7Vapk05o'
-  );
-  $video_array[]=array(
-    'video_id'=>'12',
-    'video_title'=>'Ada Apa Aja di Event PARJO 2023? | Ngobrol Bareng Founder Parjo',
-    'video_link'=>'I0dF6qHOSXg'
-  );
-  $video_array[]=array(
-    'video_id'=>'11',
-    'video_title'=>'Keunggulan Chevrolet Orlando | MPV 7 Seater Asal Amerika',
-    'video_link'=>'7_SMl9UL-Es'
   );
 ?>
 <?php require ($_SERVER['OG'].'inc/meta.php')?>

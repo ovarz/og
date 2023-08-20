c616dd8d4e7c5d31393cfd2e5d44154af225ce7e
ovarz <ovarz_03@yahoo.com>
2023-08-20 20:57:40 +0700

    update og 20

--- index.php
+++ index.php
@@ -1,10 +1,25 @@
 <?php require ('inc/base.php')?>
 <?php 
-  $video_content='7ZW9ZFtgx_E'; 
-  $video_title='Ada Mobil Baru Apa Aja di GIIAS 2023 ?'; 
-  $video_desc='OG Friends, GIIAS 2023 sudah dimulai dan OtoGadget sempat berkunjung kesana belum lama ini. Ikuti pantauan kita di #GIIAS2023 ini dimana banyak #mobilbaru dan seru muncul disana Hi OG Friends, GIIAS 2023 udah tiba, kira-kira ada Mobil Baru Apa aja di GIIAS 2023? Langsung aja tonton video kita di GIIAS 2023 ya.';
+  $video_content='zoxhipu8Nd0'; 
+  $video_title='Mobil Listrik di GIIAS 2023 DFSK, Neta - Makin Banyak Mobil Listrik China di Indonesia'; 
+  $video_desc='HI OG Friends, Kali ini kita mau membahas tentang salah satu pendatang mobil listrik dari negeri china yaitu Neta dari Hozon Group, mau tau gimana mobil listrik ini, langsung aja tonton video kita.';
   
   $video_array = array();
+  $video_array[]=array(
+    'video_id'=>'20',
+    'video_title'=>'Smart Home dan Smart Car di GIIAS 2023 by Kramat Motor',
+    'video_link'=>'j0MW82pPyNM'
+  );
+  $video_array[]=array(
+    'video_id'=>'19',
+    'video_title'=>'Ada Motor Apa Aja di GIIAS 2023 ?',
+    'video_link'=>'4sWv6xOBoBc'
+  );
+  $video_array[]=array(
+    'video_id'=>'18',
+    'video_title'=>'Ada Mobil Baru Apa Aja di GIIAS 2023 ?',
+    'video_link'=>'7ZW9ZFtgx_E'
+  );
   $video_array[]=array(
     'video_id'=>'17',
     'video_title'=>'GWM Group akan hadir di GIIAS 2023 ? Mobil Listrik China di GIIAS 2023',
@@ -14,21 +29,6 @@
     'video_id'=>'16',
     'video_title'=>'Review Suzuki Katana Tahun 96 | Jeep Tahun 90an yang Masih Menjadi Incaran Offroader',
     'video_link'=>'lbgda5vTFgk'
-  );
-  $video_array[]=array(
-    'video_id'=>'15',
-    'video_title'=>'Mobil Hybrid Honda Siap Meluncur di GIIAS 2023 ?',
-    'video_link'=>'zvfRG72kdbU'
-  );
-  $video_array[]=array(
-    'video_id'=>'14',
-    'video_title'=>'Review Hyundai Tucson Tahun 2012 | SUV Hyundai yang Menjadi Incaran Anak Millenial',
-    'video_link'=>'eO7wC4sKN5I'
-  );
-  $video_array[]=array(
-    'video_id'=>'13',
-    'video_title'=>'Mobil Baru yang ( kemungkinan ) di Jual di GIIAS 2023',
-    'video_link'=>'QcI7Vapk05o'
   );
 ?>
 <?php require ($_SERVER['OG'].'inc/meta.php')?>

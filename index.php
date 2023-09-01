2496a3ef2ab2c2550723f244357d9fcf1cc00cdd
ovarz <ovarz_03@yahoo.com>
2023-09-01 19:54:57 +0700

    4

--- index.php
+++ index.php
@@ -1,34 +1,34 @@
 <?php require ('inc/base.php')?>
 <?php 
-  $video_content='zoxhipu8Nd0'; 
-  $video_title='Mobil Listrik di GIIAS 2023 DFSK, Neta - Makin Banyak Mobil Listrik China di Indonesia'; 
-  $video_desc='HI OG Friends, Kali ini kita mau membahas tentang salah satu pendatang mobil listrik dari negeri china yaitu Neta dari Hozon Group, mau tau gimana mobil listrik ini, langsung aja tonton video kita.';
+  $video_content='NjYQDStQQ5Y'; 
+  $video_title='Benelli Patagonia Eagle Bergaya Sportster'; 
+  $video_desc='Hi OG Friends, Kali ini kita mau membahas tentang Benelli Patagonia Eagle Bergaya Sportster, kaya gimana sih impresi motor ini, langsung aja yok tonton video kita.';
   
   $video_array = array();
+  $video_array[]=array(
+    'video_id'=>'24',
+    'video_title'=>'Apa Aja Keunggulan Daihatsu Feroza ?',
+    'video_link'=>'Yss9aK_RoVY'
+  );
+  $video_array[]=array(
+    'video_id'=>'23',
+    'video_title'=>'Suzuki Katana Masih Banyak di Cari Orang ? SImak Alasannya !',
+    'video_link'=>'-nE4YvfipAw'
+  );
+  $video_array[]=array(
+    'video_id'=>'22',
+    'video_title'=>'Daihatsu Feroza Jeep Tahun 90an yang Cocok untuk Off Road',
+    'video_link'=>'06ftvFY8do4'
+  );
+  $video_array[]=array(
+    'video_id'=>'21',
+    'video_title'=>'Mobil Listrik di GIIAS 2023 DFSK, Neta - Makin Banyak Mobil Listrik China di Indonesia',
+    'video_link'=>'zoxhipu8Nd0'
+  );
   $video_array[]=array(
     'video_id'=>'20',
     'video_title'=>'Smart Home dan Smart Car di GIIAS 2023 by Kramat Motor',
     'video_link'=>'j0MW82pPyNM'
-  );
-  $video_array[]=array(
-    'video_id'=>'19',
-    'video_title'=>'Ada Motor Apa Aja di GIIAS 2023 ?',
-    'video_link'=>'4sWv6xOBoBc'
-  );
-  $video_array[]=array(
-    'video_id'=>'18',
-    'video_title'=>'Ada Mobil Baru Apa Aja di GIIAS 2023 ?',
-    'video_link'=>'7ZW9ZFtgx_E'
-  );
-  $video_array[]=array(
-    'video_id'=>'17',
-    'video_title'=>'GWM Group akan hadir di GIIAS 2023 ? Mobil Listrik China di GIIAS 2023',
-    'video_link'=>'SgccR6ZmNd8'
-  );
-  $video_array[]=array(
-    'video_id'=>'16',
-    'video_title'=>'Review Suzuki Katana Tahun 96 | Jeep Tahun 90an yang Masih Menjadi Incaran Offroader',
-    'video_link'=>'lbgda5vTFgk'
   );
 ?>
 <?php require ($_SERVER['OG'].'inc/meta.php')?>

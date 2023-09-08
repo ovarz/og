fe660d3b66fe1db2ab8e46ad954ad7bc3a54712e
ovarz <ovarz_03@yahoo.com>
2023-09-08 19:15:30 +0700

    OG

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
 <?php require ('inc/base.php')?>
 <?php 
-  $video_content='NjYQDStQQ5Y'; 
-  $video_title='Benelli Patagonia Eagle Bergaya Sportster'; 
-  $video_desc='Hi OG Friends, Kali ini kita mau membahas tentang Benelli Patagonia Eagle Bergaya Sportster, kaya gimana sih impresi motor ini, langsung aja yok tonton video kita.';
+  $video_content='XOgOruuQLg4'; 
+  $video_title='Yamaha WR 155 Custom Vintage Rasa Baru...'; 
+  $video_desc='Hi OG Friends, Kali ini kita mau membahas tentang motor Yamaha WR 155 Custom x Barokah Jaya, langsung aja tonton video kita tentang Yamaha WR 155.';
   
   $video_array = array();
+  $video_array[]=array(
+    'video_id'=>'26',
+    'video_title'=>'5 Motor Jepang Legendaris yang Menginspirasi',
+    'video_link'=>'xzizoC9ZYhk'
+  );
+  $video_array[]=array(
+    'video_id'=>'25',
+    'video_title'=>'Benelli Patagonia Eagle Bergaya Sportster',
+    'video_link'=>'NjYQDStQQ5Y'
+  );
   $video_array[]=array(
     'video_id'=>'24',
     'video_title'=>'Apa Aja Keunggulan Daihatsu Feroza ?',
@@ -19,16 +29,6 @@
     'video_id'=>'22',
     'video_title'=>'Daihatsu Feroza Jeep Tahun 90an yang Cocok untuk Off Road',
     'video_link'=>'06ftvFY8do4'
-  );
-  $video_array[]=array(
-    'video_id'=>'21',
-    'video_title'=>'Mobil Listrik di GIIAS 2023 DFSK, Neta - Makin Banyak Mobil Listrik China di Indonesia',
-    'video_link'=>'zoxhipu8Nd0'
-  );
-  $video_array[]=array(
-    'video_id'=>'20',
-    'video_title'=>'Smart Home dan Smart Car di GIIAS 2023 by Kramat Motor',
-    'video_link'=>'j0MW82pPyNM'
   );
 ?>
 <?php require ($_SERVER['OG'].'inc/meta.php')?>

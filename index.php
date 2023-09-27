3700a9663f6c0afeee2faa25766231e8b169e62d
ovarz <ovarz_03@yahoo.com>
2023-09-27 18:32:38 +0700

    update

--- index.php
+++ index.php
@@ -1,10 +1,25 @@
 <?php require ('inc/base.php')?>
 <?php 
-  $video_content='NGKHSQUtkjk'; 
-  $video_title='Toyota Vios Tahun 2013, Sedan yang Masih Menjadi Incaran Modifikator !'; 
-  $video_desc='Hi OG Friends, Kali ini kita mau membahas tentang motor Yamaha WR 155 Custom x Barokah Jaya, langsung aja tonton video kita tentang Yamaha WR 155.';
+  $video_content='d5kdVNKPpPo'; 
+  $video_title='Alasan Toyota VIOS Masih di Cari Anak Muda !'; 
+  $video_desc='HI OG Friends, Masih banyaknya orang yg membeli toyota vios tahun 2014, kira-kira kenapa masih banyak orang yg memburu mobil bekas ini ya ? langsung aja tonton video kita ya.';
   
   $video_array = array();
+  $video_array[]=array(
+    'video_id'=>'30',
+    'video_title'=>'Benelli Patagonia Eagle Bergaya British Bobber',
+    'video_link'=>'mXcjAqmrDpU'
+  );
+  $video_array[]=array(
+    'video_id'=>'29',
+    'video_title'=>'NASA Buat Mobil Listrik ? CANOO Calon Mobil Listrik Masa Depan',
+    'video_link'=>'zK5pzxSISd4'
+  );
+  $video_array[]=array(
+    'video_id'=>'28',
+    'video_title'=>'Toyota Vios Tahun 2013, Sedan yang Masih Menjadi Incaran Modifikator !',
+    'video_link'=>'NGKHSQUtkjk'
+  );
   $video_array[]=array(
     'video_id'=>'27',
     'video_title'=>'Yamaha WR 155 Custom Vintage Rasa Baru...',
@@ -14,21 +29,6 @@
     'video_id'=>'26',
     'video_title'=>'5 Motor Jepang Legendaris yang Menginspirasi',
     'video_link'=>'xzizoC9ZYhk'
-  );
-  $video_array[]=array(
-    'video_id'=>'25',
-    'video_title'=>'Benelli Patagonia Eagle Bergaya Sportster',
-    'video_link'=>'NjYQDStQQ5Y'
-  );
-  $video_array[]=array(
-    'video_id'=>'24',
-    'video_title'=>'Apa Aja Keunggulan Daihatsu Feroza ?',
-    'video_link'=>'Yss9aK_RoVY'
-  );
-  $video_array[]=array(
-    'video_id'=>'23',
-    'video_title'=>'Suzuki Katana Masih Banyak di Cari Orang ? SImak Alasannya !',
-    'video_link'=>'-nE4YvfipAw'
   );
 ?>
 <?php require ($_SERVER['OG'].'inc/meta.php')?>

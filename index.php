cca6f33481407364321c4111e214366e5c34b448
ovarz <ovarz_03@yahoo.com>
2023-09-29 21:30:57 +0700

    a

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 <?php require ('inc/base.php')?>
 <?php 
-  $video_content='d5kdVNKPpPo'; 
-  $video_title='Alasan Toyota VIOS Masih di Cari Anak Muda !'; 
-  $video_desc='HI OG Friends, Masih banyaknya orang yg membeli toyota vios tahun 2014, kira-kira kenapa masih banyak orang yg memburu mobil bekas ini ya ? langsung aja tonton video kita ya.';
+  $video_content='efsmrMWIyAQ'; 
+  $video_title='Triumph Street Scrambler 900, Motor Gahar Pelahap Segala Medan'; 
+  $video_desc='Hi OG Friends pecinta motor di Indonesia, dalam video kita kali ini, kita akan mengupas soal Triumph Street Scrambler 900. Triumph Street Scrambler 900 adalah motor klasik modern yang cocok untuk pengendara pemula maupun berpengalaman. Motor ini memiliki mesin 900cc parallel-twin yang bertenaga dan responsif, serta handling yang mudah dikendalikan. Dalam video ini, kita akan membahas secara lengkap tentang Triumph Street Scrambler 900, mulai dari spesifikasi, fitur, hingga review berkendara. Simak aja langsung ya!';
   
   $video_array = array();
+  $video_array[]=array(
+    'video_id'=>'31',
+    'video_title'=>'Alasan Toyota VIOS Masih di Cari Anak Muda !',
+    'video_link'=>'d5kdVNKPpPo'
+  );
   $video_array[]=array(
     'video_id'=>'30',
     'video_title'=>'Benelli Patagonia Eagle Bergaya British Bobber',
@@ -24,11 +29,6 @@
     'video_id'=>'27',
     'video_title'=>'Yamaha WR 155 Custom Vintage Rasa Baru...',
     'video_link'=>'XOgOruuQLg4'
-  );
-  $video_array[]=array(
-    'video_id'=>'26',
-    'video_title'=>'5 Motor Jepang Legendaris yang Menginspirasi',
-    'video_link'=>'xzizoC9ZYhk'
   );
 ?>
 <?php require ($_SERVER['OG'].'inc/meta.php')?>
